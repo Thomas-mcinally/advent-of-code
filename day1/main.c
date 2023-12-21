51c73f2c5e8fe4cc1c45460aed8159208ade76fd
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2023-12-21 08:00:42 +0000

    refactor day 1 to use shared lib

--- day1/main.c
+++ day1/main.c
@@ -3,32 +3,18 @@
 #include <string.h>
 #include <ctype.h>
 
+#include "aoc_lib.h"
 
-int read_file_to_lines(char ***lines){
-    int MAX_LINE_LENGTH = 256;
-    FILE *file = fopen("./input.txt", "r");
 
-    int lineCount = 0;
-
-    char buffer[MAX_LINE_LENGTH];
-    while (fgets(buffer, MAX_LINE_LENGTH, file) != NULL)
-    {
-        *lines = realloc(*lines, (lineCount + 1) * sizeof(char *));
-        (*lines)[lineCount] = malloc(strlen(buffer) + 1);
-
-        strcpy((*lines)[lineCount], buffer);
-        lineCount++;
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Please provide a single argument: the path to the file you want to parse\n");
+        exit(1);
     }
-    fclose(file);
-    return lineCount;
-}
 
-int main()
-{
-    FILE *file = fopen("./input.txt", "r");
-
+    char *file_path = argv[1];
     char **lines = NULL;
-    int lineCount = read_file_to_lines(&lines);
+    int lineCount = read_file_to_lines(&lines, file_path);
 
     int count = 0;
     // Iterate over each line
