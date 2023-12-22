26f3eeee81a742ef5203500d55641f4e4b286764
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2023-12-22 08:05:16 +0000

    update aoc_lib headers

--- libs/aoc_lib.h
+++ libs/aoc_lib.h
@@ -4,4 +4,10 @@
 // Declarations
 int read_file_to_lines(char ***lines, char *file_path);
 
+char *read_entire_file(char *file_path);
+
+int split_string_by_delimiter_string(const char *string_to_split, const char *delimiter_string, char ***result_strings);
+
+int string_to_lines(char *string, char ***lines);
+
 #endif // AOC_LIB_H
