b1f55d9973d729ae2bb24711d7a48fdebd6dca68
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2023-12-24 11:47:34 +0000

    remove useless comment

--- day1/main.c
+++ day1/main.c
@@ -4,11 +4,7 @@
 #include <ctype.h>
 
 #include "aoc_lib.h"
-// The newly-improved calibration document consists of lines of text; each line originally contained a 
-// specific calibration value that the Elves now need to recover. On each line, the calibration value can 
-// be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
 
-// What is the sum of all the calibration values?
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "Please provide a single argument: the path to the file you want to parse\n");
@@ -20,7 +16,7 @@ int main(int argc, char **argv) {
     int lineCount = read_file_to_lines(&lines, file_path);
 
     int count = 0;
-    // Iterate over each line
+
     for (int i = 0; i < lineCount-1; i++)
     {
         int j = 0;
