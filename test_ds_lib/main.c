b4a99de7d6dfdefd73ae3d9a5cddd90687832c4c
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2023-12-25 13:31:48 +0100

    test dynamic array of structs

--- test_ds_lib/main.c
+++ test_ds_lib/main.c
@@ -7,6 +7,11 @@ typedef struct {
     int value;
 } Map_Element;
 
+typedef struct {
+    int x;
+    int y;
+} Coordinates;
+
 int main() {
     // Test hashmap
     Map_Element *map = NULL;
@@ -23,4 +28,16 @@ int main() {
     printf("arr[1]: %d\n", arr[1]);
     printf("arr[2]: %d\n", arr[2]);
     arrfree(arr);
+
+    // test dynamic array of structs
+    Coordinates *coords = NULL;
+    Coordinates my_point = {1, 100};
+    Coordinates my_point2 = {2, 200};
+    arrput(coords, my_point);
+    arrput(coords, my_point2);
+    printf("coords[0].x: %d\n", coords[0].x);
+    printf("coords[0].y: %d\n", coords[0].y);
+    printf("coords[1].x: %d\n", coords[1].x);
+    printf("coords[1].y: %d\n", coords[1].y);
+    arrfree(coords);
 };
