71a763c88a03e50b6954e7feee758ed069f04591
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2023-12-25 11:04:55 +0100

    refactor day1 part2 - read_string_digit

--- day1/main2.c
+++ day1/main2.c
@@ -5,32 +5,18 @@
 
 #include "aoc_lib.h"
 
-int read_string_digit_starting_at(char *string)
+int read_string_digit(char *string, int is_start)
 {
-    if (strncmp(string, "zero", 4) == 0) return 0;
-    if (strncmp(string, "one", 3) == 0) return 1;
-    if (strncmp(string, "two", 3) == 0) return 2;
-    if (strncmp(string, "three", 5) == 0) return 3;
-    if (strncmp(string, "four", 4) == 0) return 4;
-    if (strncmp(string, "five", 4) == 0) return 5;
-    if (strncmp(string, "six", 3) == 0) return 6;
-    if (strncmp(string, "seven", 5) == 0) return 7;
-    if (strncmp(string, "eight", 5) == 0) return 8;
-    if (strncmp(string, "nine", 4) == 0) return 9;
-    return -1;
-}
-int read_string_digit_ending_at(char *string)
-{
-    if (strncmp(string-3, "zero", 4) == 0) return 0;
-    if (strncmp(string-2, "one", 3) == 0) return 1;
-    if (strncmp(string-2, "two", 3) == 0) return 2;
-    if (strncmp(string-4, "three", 5) == 0) return 3;
-    if (strncmp(string-3, "four", 4) == 0) return 4;
-    if (strncmp(string-3, "five", 4) == 0) return 5;
-    if (strncmp(string-2, "six", 3) == 0) return 6;
-    if (strncmp(string-4, "seven", 5) == 0) return 7;
-    if (strncmp(string-4, "eight", 5) == 0) return 8;
-    if (strncmp(string-3, "nine", 4) == 0) return 9;
+    if (strncmp(is_start==1?string:string-3, "zero", 4) == 0) return 0;
+    if (strncmp(is_start==1?string:string-2, "one", 3) == 0) return 1;
+    if (strncmp(is_start==1?string:string-2, "two", 3) == 0) return 2;
+    if (strncmp(is_start==1?string:string-4, "three", 5) == 0) return 3;
+    if (strncmp(is_start==1?string:string-3, "four", 4) == 0) return 4;
+    if (strncmp(is_start==1?string:string-3, "five", 4) == 0) return 5;
+    if (strncmp(is_start==1?string:string-2, "six", 3) == 0) return 6;
+    if (strncmp(is_start==1?string:string-4, "seven", 5) == 0) return 7;
+    if (strncmp(is_start==1?string:string-4, "eight", 5) == 0) return 8;
+    if (strncmp(is_start==1?string:string-3, "nine", 4) == 0) return 9;
     return -1;
 }
 
@@ -56,7 +42,7 @@ int main(int argc, char **argv) {
                 count += (lines[i][j] - '0') * 10;
                 break;
             }
-            int string_digit = read_string_digit_starting_at(&lines[i][j]);
+            int string_digit = read_string_digit(&lines[i][j], 1);
             if (string_digit != -1)
             {
                 count += string_digit * 10;
@@ -73,8 +59,7 @@ int main(int argc, char **argv) {
                 count += lines[i][j] - '0';
                 break;
             }
-            int string_digit = read_string_digit_ending_at(&lines[i][j]);
-            if (i==2) printf("reverse string digit result: %i\n", string_digit);
+            int string_digit = read_string_digit(&lines[i][j], 0);
             if (string_digit != -1)
             {
                 count += string_digit;
