13e3bafcd5e4fc6ff2c3023be43d3fff04d70eb9
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2023-12-29 03:41:12 +0100

    refactor day7 calculate_score_from_buckets and sort_hands_into_buckets helper functions

--- day7/main.c
+++ day7/main.c
@@ -38,6 +38,49 @@ void order_hands(char *hands[], size_t num_hands) {
     qsort(hands, num_hands, sizeof(hands[0]), custom_compare);
 }
 
+void sort_hands_into_buckets(char **hands, size_t num_hands, char **buckets[]) {
+    for (int i=0; i<num_hands; i++) {
+        int counts[13] = {0};
+        int three_of_a_kind = 0;
+        int two_of_a_kind = 0;
+        int four_of_a_kind = 0;
+        int five_of_a_kind = 0;
+        for (int j=0; j<5; j++) {
+            int index = strchr(order_of_chars, hands[i][j]) - order_of_chars;
+            counts[index]++;
+            if (counts[index] == 2) two_of_a_kind++;
+            else if (counts[index] == 3) {
+                three_of_a_kind++;
+                two_of_a_kind--;
+            }
+            else if (counts[index] == 4) four_of_a_kind++;
+            else if (counts[index] == 5) five_of_a_kind++;
+        }
+        if (five_of_a_kind) arrput(buckets[0], hands[i]);
+        else if (four_of_a_kind) arrput(buckets[1], hands[i]);
+        else if (three_of_a_kind && two_of_a_kind) arrput(buckets[2], hands[i]);
+        else if (three_of_a_kind) arrput(buckets[3], hands[i]);
+        else if (two_of_a_kind == 2) arrput(buckets[4], hands[i]);
+        else if (two_of_a_kind) arrput(buckets[5], hands[i]);
+        else arrput(buckets[6], hands[i]);
+    }
+}
+
+long long unsigned int calculate_score_from_buckets(char **buckets[]) {
+    long long unsigned int total = 0;
+    int rank = 1000;
+    for (int i=0; i<7; i++) {
+        if (*buckets[i] == NULL) continue;
+        order_hands(buckets[i], arrlen(buckets[i]));
+        
+        for (int j=0; j<arrlen(buckets[i]); j++) {
+            int bid = extract_number_from_string(buckets[i][j]+6);
+            total += bid * rank;
+            rank--;
+        }
+    }
+    return total;
+}
 
 int main(int argc, char **argv)
 {
@@ -53,54 +96,15 @@ int main(int argc, char **argv)
 
     char **buckets[7] = {NULL};
 
-    for (int i=0; i<linecount; i++)
-    {
-        int counts[13] = {0};
-        int three_of_a_kind = 0;
-        int two_of_a_kind = 0;
-        int four_of_a_kind = 0;
-        int five_of_a_kind = 0;
-        for (int j=0; j<5; j++)
-        {
-            int index = strchr(order_of_chars, lines[i][j]) - order_of_chars;
-            counts[index]++;
-            if (counts[index] == 2) two_of_a_kind++;
-            else if (counts[index] == 3) {
-                three_of_a_kind++;
-                two_of_a_kind--;
-            }
-            else if (counts[index] == 4) four_of_a_kind++;
-            else if (counts[index] == 5) five_of_a_kind++;
-        }
-        if (five_of_a_kind) arrput(buckets[0], lines[i]);
-        else if (four_of_a_kind) arrput(buckets[1], lines[i]);
-        else if (three_of_a_kind && two_of_a_kind) arrput(buckets[2], lines[i]);
-        else if (three_of_a_kind) arrput(buckets[3], lines[i]);
-        else if (two_of_a_kind == 2) arrput(buckets[4], lines[i]);
-        else if (two_of_a_kind) arrput(buckets[5], lines[i]);
-        else arrput(buckets[6], lines[i]);
-    }
+    sort_hands_into_buckets(lines, linecount, buckets);
 
-    long long unsigned int total = 0;
-    int rank = 1000;
-    for (int i=0; i<7; i++)
-    {
-        if (*buckets[i] == NULL) continue;
-        order_hands(buckets[i], arrlen(buckets[i]));
-        
-        for (int j=0; j<arrlen(buckets[i]); j++)
-        {
-            int bid = extract_number_from_string(buckets[i][j]+6);
-            total += bid * rank;
-            rank--;
-            free(buckets[i][j]);
-        }
-        arrfree(buckets[i]);
-    }
-    free(lines);
+    long long unsigned int total = calculate_score_from_buckets(buckets);
 
     printf("part1: %llu\n", total);
 
 
+    for (int i=0; i<7; i++) arrfree(buckets[i]);
+    for (int i=0; i<linecount; i++) free(lines[i]);
+    free(lines);
     return 0;
 }
