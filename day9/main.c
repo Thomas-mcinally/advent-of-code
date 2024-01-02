55d392bb4ac884061fd028a7bb873e3d2e76cf45
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-02 19:16:40 +0000

    refactor day9 - use recursion for increased readability

--- day9/main.c
+++ day9/main.c
@@ -7,44 +7,23 @@
 #include "stb_ds.h"
 
 int extrapolate_next_val(int *nums, int num_count, bool is_part1){
-    int *operands = NULL;
-    int *cur_arr = NULL;
-    int *next_arr = NULL;
-
-    for (int i=0; i<num_count; i++) arrput(cur_arr, nums[i]);
-
-    bool is_next_arr_all_zeroes = false;
-    while (!is_next_arr_all_zeroes) {
-        if (is_part1) arrpush(operands, arrlast(cur_arr));
-        else arrpush(operands, cur_arr[0]);
-
-        is_next_arr_all_zeroes = true;
-        for (int i = 1; i < arrlen(cur_arr); i++)
-        {
-            int diff = cur_arr[i] - cur_arr[i-1];
-            if (diff != 0) is_next_arr_all_zeroes = false;
-            arrput(next_arr, diff);
-        }
-        arrfree(cur_arr);
-        cur_arr = next_arr;
-        next_arr = NULL;
-    }
-    arrfree(next_arr);
-    arrfree(cur_arr);
-
-
-    while (arrlen(operands) > 1) {
-        int b = arrpop(operands);
-        int a = arrpop(operands);
-        if (is_part1) arrpush(operands, a+b);
-        else arrpush(operands, a-b);
+    int *diff_array = calloc(num_count-1, sizeof(int));
+    bool is_next_arr_all_zeroes = true;
+    for (int i = 1; i < num_count; i++)
+    {
+        int diff = nums[i] - nums[i-1];
+        if (diff != 0) is_next_arr_all_zeroes = false;
+        diff_array[i-1] = diff;
     }
 
-    int result = operands[0];
-    arrfree(operands);
+    int next_result =  is_next_arr_all_zeroes ? next_result = 0 : extrapolate_next_val(diff_array, num_count-1, is_part1);
+    free(diff_array);
 
-    return result;
+
+    if (is_part1) return nums[num_count-1] + next_result;
+    else return nums[0] - next_result;
 }
+
 
 int **split_string_lines_into_int_lines(char **string_lines, int linecount, char *separator)
 // output nums must be freed by caller
