3aacc6ae65babd8cf6feccdf7930b2f832a7957c
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-07 17:43:04 +0000

    refactor day13 part1 in prep for part2

--- day13/main.py
+++ day13/main.py
@@ -1,23 +1,24 @@
-def is_valid_palindrome_centered_at(s, l,r):
-    while l >= 0 and r < len(s) and s[l] == s[r]:
+def smudges_required_for_valid_palindrome_centered_at(s, l,r):
+    smudges = 0
+    while l >= 0 and r < len(s):
+        if s[l] != s[r]:
+            smudges += 1
         l-=1
         r+=1
-    if l == -1 or r == len(s): return True
-    return False
+    return smudges
 
 def score_from_chunk(chunk, score_multiplier):
     ROWS = len(chunk)
     COLS = len(chunk[0])
-    possible_vertical_mirror = [True] * (COLS-1)
-    for r in range(ROWS):
-          for c in range(COLS-1):
-            if not possible_vertical_mirror[c]: continue
-            possible_vertical_mirror[c] = is_valid_palindrome_centered_at(chunk[r], c, c+1)
 
     score = 0
-    for i in range(len(possible_vertical_mirror)):
-        if possible_vertical_mirror[i]:
-            score += (i+1) * score_multiplier
+    for c in range(COLS-1):
+
+        for r in range(ROWS):
+            if smudges_required_for_valid_palindrome_centered_at(chunk[r], c, c+1) != 0:
+                break
+        else:
+            score += (c+1) * score_multiplier
     return score
 
 
