07185a51797c1b35c50eb95f4ccfef297df606ef
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-07 10:00:30 +0000

    refactor day12 python

--- day12/main.py
+++ day12/main.py
@@ -28,12 +28,9 @@ for line in input:
     s, key = line.split(" ")
     key = [int(x) for x in key.split(",")]
 
-    dot_s_count = [0] * len(s)
-    for i,char in enumerate(s):
-        if char == ".":
-            dot_s_count[i] = 1
-        if i > 0:
-            dot_s_count[i] += dot_s_count[i-1]
+    dot_s_count = [1 if char == "." else 0 for char in s]
+    for i in range(1,len(dot_s_count)):
+        dot_s_count[i] += dot_s_count[i-1]
 
     total += dfs(s, key, 0, 0, dot_s_count, {})
 
@@ -41,12 +38,9 @@ for line in input:
 
     s_part_2 = "?".join([s]*5)
     key_part_2 = key*5
-    dot_s_count_part_2 = [0] * len(s_part_2)
-    for i,char in enumerate(s_part_2):
-        if char == ".":
-            dot_s_count_part_2[i] = 1
-        if i > 0:
-            dot_s_count_part_2[i] += dot_s_count_part_2[i-1]
+    dot_s_count_part_2 = [1 if char == "." else 0 for char in s_part_2]
+    for i in range(1,len(dot_s_count_part_2)):
+        dot_s_count_part_2[i] += dot_s_count_part_2[i-1]
     total_2 += dfs(s_part_2, key_part_2, 0, 0, dot_s_count_part_2, {})
 print(total)
 print(total_2)
