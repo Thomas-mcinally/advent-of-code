dba66c356aa8afbc8e6f5eae866897f9961679a2
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-07 10:03:28 +0000

    refactor day12 to use defaultdict

--- day12/main.py
+++ day12/main.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def dfs(s: str, key: list[int], i: int, j: int, dot_count_s: list[int], cache: dict) -> int:
     if i >= len(s) and j==len(key):
         return 1
@@ -12,13 +15,12 @@ def dfs(s: str, key: list[int], i: int, j: int, dot_count_s: list[int], cache: d
     if (i,j) in cache:
         return cache[(i,j)]
 
-    res = 0
+
     if s[i] != "." and dot_count_s[i] == dot_count_s[i+key[j]-1] and (i+key[j] == len(s) or s[i+key[j]] != "#"):
-        res += dfs(s, key, i+key[j]+1, j+1, dot_count_s, cache)
+        cache[(i,j)] += dfs(s, key, i+key[j]+1, j+1, dot_count_s, cache)
     if s[i] != "#":
-        res += dfs(s, key, i+1, j, dot_count_s, cache)
-    cache[(i,j)] = res
-    return res
+        cache[(i,j)] += dfs(s, key, i+1, j, dot_count_s, cache)
+    return cache[(i,j)]
 
 with open("input.txt", "r") as file:
     input = file.read().splitlines()
@@ -32,7 +34,7 @@ for line in input:
     for i in range(1,len(dot_s_count)):
         dot_s_count[i] += dot_s_count[i-1]
 
-    total += dfs(s, key, 0, 0, dot_s_count, {})
+    total += dfs(s, key, 0, 0, dot_s_count, defaultdict(int))
 
 
 
@@ -41,7 +43,7 @@ for line in input:
     dot_s_count_part_2 = [1 if char == "." else 0 for char in s_part_2]
     for i in range(1,len(dot_s_count_part_2)):
         dot_s_count_part_2[i] += dot_s_count_part_2[i-1]
-    total_2 += dfs(s_part_2, key_part_2, 0, 0, dot_s_count_part_2, {})
+    total_2 += dfs(s_part_2, key_part_2, 0, 0, dot_s_count_part_2, defaultdict(int))
 print(total)
 print(total_2)
 
