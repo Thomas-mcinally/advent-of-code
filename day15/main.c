16f11a784945cde89dc94dbb5265867c1e95d7a8
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-09 08:24:27 +0000

    Revert "refactor day15"
    This reverts commit 1f34f8bf0d8905efd2017708fd8074f42ecb08a2.

--- day15/main.c
+++ day15/main.c
@@ -15,31 +15,31 @@ int main(int argc, char **argv)
     char *file_path = argv[1];
     char *file_contents = read_entire_file(file_path);
 
-    char **codes = NULL;
-    int num_codes = split_string_by_delimiter_string(file_contents, ",", &codes);
 
+    size_t i = 0;
+    size_t current_value = 0;
     size_t total_value = 0;
-
-    for (int i=0; i<num_codes; i++){
-        size_t current_value = 0;
-        int j = 0;
-        while (codes[i][j] != '\0')
+    while (file_contents[i] != '\0')
+    {
+        if (file_contents[i] == '\n')
         {
-            if (codes[i][j] == '\n')
-            {
-                printf("found unexpected newline at i: %d, j: %d\n", i, j);
-                exit(1);
-            }
-            else {
-                current_value += codes[i][j];
-                current_value *= 17;
-                current_value %= 256;
-
-            }
-            j += 1;
+            printf("found unexpected newline at index: %zu\n", i);
+            exit(1);
         }
-        total_value += current_value;
+        else if (file_contents[i] == ',') {
+            total_value += current_value;
+            current_value = 0;
+        }
+        else {
+            current_value += file_contents[i];
+            current_value *= 17;
+            current_value %= 256;
+
+        }
+
+        i += 1;
     }
+    total_value += current_value;
 
     printf("part1 sol: %zu\n", total_value);
 
