b0658ecc271570940aaa4af3b960e39938399637
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-12 07:47:12 +0000

    refactor - introduce direction argument to DFS

--- day16/main.c
+++ day16/main.c
@@ -9,110 +9,113 @@ const int UP = 1 << 1;
 const int RIGHT = 1 << 2;
 const int LEFT = 1 << 3;
 
-void dfs(char **grid, int ROWS, int COLS, int *visited, int cur_r, int cur_c, int prev_r, int prev_c)
+void dfs(char **grid, int ROWS, int COLS, int *visited, int cur_r, int cur_c, int direction)
 {
-    if (cur_r < 0 || cur_r >= ROWS || cur_c < 0 || cur_c >= COLS)
+    if (cur_r < 0 || cur_r >= ROWS || cur_c < 0 || cur_c >= COLS || visited[cur_r*COLS + cur_c]&direction)
         return;
 
-    int is_travelling_down = (cur_r == prev_r + 1);
-    int is_travelling_up = (cur_r == prev_r - 1);
-    int is_travelling_right = (cur_c == prev_c + 1);
-    int is_travelling_left = (cur_c == prev_c - 1);
-
-
-    if (is_travelling_down && (visited[cur_r * COLS + cur_c] & DOWN))
-        return;
-    if (is_travelling_up && (visited[cur_r * COLS + cur_c] & UP))
-        return;
-    if (is_travelling_right && (visited[cur_r * COLS + cur_c] & RIGHT))
-        return;
-    if (is_travelling_left && (visited[cur_r * COLS + cur_c] & LEFT))
-        return;
-
-
-    if (is_travelling_down) visited[cur_r * COLS + cur_c] |= DOWN;
-    if (is_travelling_up) visited[cur_r * COLS + cur_c] |= UP;
-    if (is_travelling_right) visited[cur_r * COLS + cur_c] |= RIGHT;
-    if (is_travelling_left) visited[cur_r * COLS + cur_c] |= LEFT;
+    visited[cur_r*COLS + cur_c] |= direction;
 
     if (grid[cur_r][cur_c] == '.')
     {
         int next_r, next_c;
-        if (is_travelling_down)
+        if (direction == DOWN)
             next_r = cur_r + 1, next_c = cur_c;
-        else if (is_travelling_up)
+        else if (direction == UP)
             next_r = cur_r - 1, next_c = cur_c;
-        else if (is_travelling_right)
+        else if (direction == RIGHT)
             next_r = cur_r, next_c = cur_c + 1;
-        else if (is_travelling_left)
+        else if (direction == LEFT)
             next_r = cur_r, next_c = cur_c - 1;
-        dfs(grid, ROWS, COLS, visited, next_r, next_c, cur_r, cur_c);
+        dfs(grid, ROWS, COLS, visited, next_r, next_c, direction);
         return;
     }
     if (grid[cur_r][cur_c] == '/')
     {
         int next_r, next_c;
-        if (is_travelling_down)
-            next_r = cur_r, next_c = cur_c - 1;
-        else if (is_travelling_up)
-            next_r = cur_r, next_c = cur_c + 1;
-        else if (is_travelling_right)
-            next_r = cur_r - 1, next_c = cur_c;
-        else if (is_travelling_left)
-            next_r = cur_r + 1, next_c = cur_c;
-        dfs(grid, ROWS, COLS, visited, next_r, next_c, cur_r, cur_c);
+        int next_direction;
+        if (direction == DOWN){
+            next_r = cur_r;
+            next_c = cur_c - 1;
+            next_direction = LEFT;
+        }
+        else if (direction == UP){
+            next_r = cur_r;
+            next_c = cur_c + 1;
+            next_direction = RIGHT;
+        }
+        else if (direction == RIGHT){
+            next_r = cur_r - 1;
+            next_c = cur_c;
+            next_direction = UP;
+        }
+        else if (direction == LEFT){
+            next_r = cur_r + 1;
+            next_c = cur_c;
+            next_direction = DOWN;
+        }
+        dfs(grid, ROWS, COLS, visited, next_r, next_c, next_direction);
         return;
     }
 
     if (grid[cur_r][cur_c] == '\\')
     {
         int next_r, next_c;
-        if (is_travelling_down)
+        int next_direction;
+        if (direction == DOWN){
             next_r = cur_r, next_c = cur_c + 1;
-        else if (is_travelling_up)
+            next_direction = RIGHT;
+        }
+        else if (direction == UP){
             next_r = cur_r, next_c = cur_c - 1;
-        else if (is_travelling_right)
+            next_direction = LEFT;
+        }
+        else if (direction == RIGHT){
             next_r = cur_r + 1, next_c = cur_c;
-        else if (is_travelling_left)
+            next_direction = DOWN;
+        }
+        else if (direction == LEFT){
             next_r = cur_r - 1, next_c = cur_c;
-        dfs(grid, ROWS, COLS, visited, next_r, next_c, cur_r, cur_c);
+            next_direction = UP;
+        }
+        dfs(grid, ROWS, COLS, visited, next_r, next_c, next_direction);
         return;
     }
 
     if (grid[cur_r][cur_c] == '|')
     {
-        if (is_travelling_left || is_travelling_right)
+        if (direction==LEFT || direction==RIGHT)
         {
-            dfs(grid, ROWS, COLS, visited, cur_r + 1, cur_c, cur_r, cur_c);
-            dfs(grid, ROWS, COLS, visited, cur_r - 1, cur_c, cur_r, cur_c);
+            dfs(grid, ROWS, COLS, visited, cur_r + 1, cur_c, DOWN);
+            dfs(grid, ROWS, COLS, visited, cur_r - 1, cur_c, UP);
         }
-        else if (is_travelling_up)
-            dfs(grid, ROWS, COLS, visited, cur_r - 1, cur_c, cur_r, cur_c);
-        else if (is_travelling_down)
-            dfs(grid, ROWS, COLS, visited, cur_r + 1, cur_c, cur_r, cur_c);
+        else if (direction==UP)
+            dfs(grid, ROWS, COLS, visited, cur_r - 1, cur_c, UP);
+        else if (direction==DOWN)
+            dfs(grid, ROWS, COLS, visited, cur_r + 1, cur_c, DOWN);
 
         return;
     }
 
     if (grid[cur_r][cur_c] == '-')
     {
-        if (is_travelling_up || is_travelling_down)
+        if (direction==UP || direction==DOWN)
         {
-            dfs(grid, ROWS, COLS, visited, cur_r, cur_c + 1, cur_r, cur_c);
-            dfs(grid, ROWS, COLS, visited, cur_r, cur_c - 1, cur_r, cur_c);
+            dfs(grid, ROWS, COLS, visited, cur_r, cur_c + 1, RIGHT);
+            dfs(grid, ROWS, COLS, visited, cur_r, cur_c - 1, LEFT);
         }
-        else if (is_travelling_left)
-            dfs(grid, ROWS, COLS, visited, cur_r, cur_c - 1, cur_r, cur_c);
-        else if (is_travelling_right)
-            dfs(grid, ROWS, COLS, visited, cur_r, cur_c + 1, cur_r, cur_c);
+        else if (direction==LEFT)
+            dfs(grid, ROWS, COLS, visited, cur_r, cur_c - 1, LEFT);
+        else if (direction==RIGHT)
+            dfs(grid, ROWS, COLS, visited, cur_r, cur_c + 1, RIGHT);
         return;
     }
 }
 
-int find_energized_tiles_given_starting_pos(char **grid, int ROWS, int COLS, int start_r, int start_c, int prev_r, int prev_c)
+int find_energized_tiles_given_starting_pos(char **grid, int ROWS, int COLS, int start_r, int start_c, int start_direction)
 {
     int *visited = calloc(ROWS * COLS, sizeof(int));
-    dfs(grid, ROWS, COLS, visited, start_r, start_c, prev_r, prev_c);
+    dfs(grid, ROWS, COLS, visited, start_r, start_c, start_direction);
 
     int count = 0;
     for (int i = 0; i < ROWS * COLS; i++)
@@ -137,18 +140,18 @@ int main(int argc, char **argv)
     int ROWS = read_file_to_lines(&grid, file_path);
     int COLS = strlen(grid[0]);
 
-    printf("part1 sol: %d\n", find_energized_tiles_given_starting_pos(grid, ROWS, COLS, 0, 0, 0, -1));
+    printf("part1 sol: %d\n", find_energized_tiles_given_starting_pos(grid, ROWS, COLS, 0, 0, RIGHT));
 
     int max_count = 0;
     for (int r = 0; r < ROWS; r++)
     {
-        max_count = max(max_count, find_energized_tiles_given_starting_pos(grid, ROWS, COLS, r, 0, r, -1));
-        max_count = max(max_count, find_energized_tiles_given_starting_pos(grid, ROWS, COLS, r, COLS - 1, r, COLS));
+        max_count = max(max_count, find_energized_tiles_given_starting_pos(grid, ROWS, COLS, r, 0, RIGHT));
+        max_count = max(max_count, find_energized_tiles_given_starting_pos(grid, ROWS, COLS, r, COLS - 1, LEFT));
     }
     for (int c = 0; c < COLS; c++)
     {
-        max_count = max(max_count, find_energized_tiles_given_starting_pos(grid, ROWS, COLS, 0, c, -1, c));
-        max_count = max(max_count, find_energized_tiles_given_starting_pos(grid, ROWS, COLS, ROWS - 1, c, ROWS, c));
+        max_count = max(max_count, find_energized_tiles_given_starting_pos(grid, ROWS, COLS, 0, c, DOWN));
+        max_count = max(max_count, find_energized_tiles_given_starting_pos(grid, ROWS, COLS, ROWS - 1, c, UP));
     }
 
     printf("part2 sol: %d\n", max_count);
