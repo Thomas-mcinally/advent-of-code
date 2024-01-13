c09daf1f9bbbbc04753f0bf4ae5d89ed7fedd0b7
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-13 17:51:31 +0000

    refactor day18 - reuse variables from part1 in part2

--- day18/main.c
+++ day18/main.c
@@ -30,11 +30,11 @@ int main(int argc, char **argv)
     Point *points = malloc((linecount + 1) * sizeof(Point));
     Point start = {0, 0};
     points[0] = start;
-    int edge_count = 0;
+    size_t edge_count = 0;
     for (int i = 0; i < linecount; i++)
     {
         char dir = lines[i][0];
-        int val = extract_number_from_string(lines[i] + 2);
+        size_t val = extract_number_from_string(lines[i] + 2);
         edge_count += val;
         points[i+1] = points[i];
 
@@ -54,17 +54,17 @@ int main(int argc, char **argv)
         }
     }
 
-    int shoestring_area = 0;
+    size_t shoestring_area = 0;
     for (int i=0; i < linecount; i++) shoestring_area += det(points[i], points[i+1]);
-    printf("part1 solution: %d\n", shoestring_area/2 + edge_count/2 + 1);
+    printf("part1 solution: %zu\n", shoestring_area/2 + edge_count/2 + 1);
 
 
-    size_t edge_count2 = 0;
+    edge_count = 0;
     for (int i=0; i<linecount; i++){
         char *hex_start = strchr(lines[i], '#');
         size_t val = extract_hex_val_from_string(hex_start + 1, 5);
         char dir2 = *(hex_start + 6);
-        edge_count2 += val;
+        edge_count += val;
         points[i+1] = points[i];
         
         switch(dir2){
@@ -83,9 +83,9 @@ int main(int argc, char **argv)
         }
     }
 
-    size_t shoestring_area2 = 0;
-    for (int i=0; i < linecount; i++) shoestring_area2 += det(points[i], points[i+1]);
-    printf("part2 solution: %zu\n", shoestring_area2/2 + edge_count2/2 + 1);
+    shoestring_area = 0;
+    for (int i=0; i < linecount; i++) shoestring_area += det(points[i], points[i+1]);
+    printf("part2 solution: %zu\n", shoestring_area/2 + edge_count/2 + 1);
 
 
     free(points);
