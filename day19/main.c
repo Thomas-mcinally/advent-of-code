5143283d449e32767c4f6ad982dd03f5d0f287f9
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-14 14:01:07 +0000

    refactor day19 - separate function for part1

--- day19/main.c
+++ day19/main.c
@@ -16,6 +16,46 @@ typedef struct
     char *value;
 } WorkflowsMapItem;
 
+size_t part1(WorkflowsMapItem *workflows, int **items, int item_count){
+    size_t total = 0;
+    for (int i=0; i<item_count; i++){
+        int cur_x = items[i][0];
+        int cur_m = items[i][1];
+        int cur_a = items[i][2];
+        int cur_s = items[i][3];
+        char cur[4]= {'i', 'n', '\0', '\0'};
+        while (cur[0] != 'R' && cur[0] != 'A'){
+            char *rules = shget(workflows, &cur);
+            while (1){
+                char *rule_end = strchr(rules, ',');
+                if (rule_end == NULL) {
+                    strcpy(cur, rules); 
+                    break;
+                }
+                char c = rules[0];
+                char cond = rules[1];
+                int val = extract_number_from_string(rules+2);
+                char *dest_start = strchr(rules, ':') + 1;
+                rules = rule_end + 1;
+
+                if ((c == 'x' && cond == '<' && cur_x < val)
+                || (c == 'x' && cond == '>' && cur_x > val)
+                || (c == 'm' && cond == '<' && cur_m < val)
+                || (c == 'm' && cond == '>' && cur_m > val)
+                || (c == 'a' && cond == '<' && cur_a < val)
+                || (c == 'a' && cond == '>' && cur_a > val)
+                || (c == 's' && cond == '<' && cur_s < val)
+                || (c == 's' && cond == '>' && cur_s > val)){
+                    strncpy(cur, dest_start, rule_end - dest_start);
+                    cur[rule_end - dest_start] = '\0';
+                    break;
+                }
+            }
+        }
+        if (cur[0] == 'A') total += cur_x + cur_m + cur_a + cur_s;
+    }
+    return total;
+}
 size_t part2(WorkflowsMapItem *workflows, char *cur, int x_start, int x_end, int m_start, int m_end, int a_start, int a_end, int s_start, int s_end){
     if (x_end < x_start || m_end < m_start || a_end < a_start || s_end < s_start || *cur == 'R') return 0;
     if (*cur == 'A') {
@@ -121,46 +161,8 @@ int main(int argc, char **argv)
         items[i][3] = extract_number_from_string_starting_from(item_lines[i], &j);
     }
 
-    // calculate part1
-    int total = 0;
-    for (int i=0; i<item_count; i++){
-        int cur_x = items[i][0];
-        int cur_m = items[i][1];
-        int cur_a = items[i][2];
-        int cur_s = items[i][3];
-        char cur[4]= {'i', 'n', '\0', '\0'};
-        while (cur[0] != 'R' && cur[0] != 'A'){
-            char *rules = shget(workflows, &cur);
-            while (1){
-                char *rule_end = strchr(rules, ',');
-                if (rule_end == NULL) {
-                    strcpy(cur, rules); 
-                    break;
-                }
-                char c = rules[0];
-                char cond = rules[1];
-                int val = extract_number_from_string(rules+2);
-                char *dest_start = strchr(rules, ':') + 1;
-                rules = rule_end + 1;
 
-                if ((c == 'x' && cond == '<' && cur_x < val)
-                || (c == 'x' && cond == '>' && cur_x > val)
-                || (c == 'm' && cond == '<' && cur_m < val)
-                || (c == 'm' && cond == '>' && cur_m > val)
-                || (c == 'a' && cond == '<' && cur_a < val)
-                || (c == 'a' && cond == '>' && cur_a > val)
-                || (c == 's' && cond == '<' && cur_s < val)
-                || (c == 's' && cond == '>' && cur_s > val)){
-                    strncpy(cur, dest_start, rule_end - dest_start);
-                    cur[rule_end - dest_start] = '\0';
-                    break;
-                }
-            }
-        }
-        if (cur[0] == 'A') total += cur_x + cur_m + cur_a + cur_s;
-    }
-    printf("Part 1: %i\n", total);
-
+    printf("Part 1: %zu\n", part1(workflows, items, item_count));
     printf("Part 2: %zu\n", part2(workflows, "in", 1, 4000, 1, 4000, 1, 4000, 1, 4000));
 
     for (int i = 0; i < section_count; i++) free(sections[i]);
