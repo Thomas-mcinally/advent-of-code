3f4d61d761e2acf6481423779c1941b996aa5e78
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-21 12:40:51 +0000

    day23 part2 - backtracking

--- day23/main.c
+++ day23/main.c
@@ -6,7 +6,7 @@
 #include "aoc_lib.h"
 
 //DFS backtracking solution
-void backtrack(char **grid, int ROWS, int COLS, int r, int c, size_t pathlength, size_t *longest_pathlength){
+void backtrack_part1(char **grid, int ROWS, int COLS, int r, int c, size_t pathlength, size_t *longest_pathlength){
     if(r<0 || r>=ROWS || c<0 || c>=COLS || grid[r][c] == '#') return;
 
     if (r == ROWS-1 && c == COLS-2){
@@ -22,17 +22,33 @@ void backtrack(char **grid, int ROWS, int COLS, int r, int c, size_t pathlength,
         else if (grid[r][c] == 'v') next_r = r+1, next_c = c;
         char temp = grid[r][c];
         grid[r][c] = '#';
-        backtrack(grid, ROWS, COLS, next_r, next_c, pathlength+1, longest_pathlength);
+        backtrack_part1(grid, ROWS, COLS, next_r, next_c, pathlength+1, longest_pathlength);
         grid[r][c] = temp;
     }
     else if (grid[r][c] == '.') {
         grid[r][c] = '#';
-        backtrack(grid, ROWS, COLS, r, c+1, pathlength+1, longest_pathlength);
-        backtrack(grid, ROWS, COLS, r, c-1, pathlength+1, longest_pathlength);
-        backtrack(grid, ROWS, COLS, r+1, c, pathlength+1, longest_pathlength);
-        backtrack(grid, ROWS, COLS, r-1, c, pathlength+1, longest_pathlength);
+        backtrack_part1(grid, ROWS, COLS, r, c+1, pathlength+1, longest_pathlength);
+        backtrack_part1(grid, ROWS, COLS, r, c-1, pathlength+1, longest_pathlength);
+        backtrack_part1(grid, ROWS, COLS, r+1, c, pathlength+1, longest_pathlength);
+        backtrack_part1(grid, ROWS, COLS, r-1, c, pathlength+1, longest_pathlength);
         grid[r][c] = '.';
     }
+}
+
+void backtrack_part2(char **grid, int ROWS, int COLS, int r, int c, size_t pathlength, size_t *longest_pathlength){
+    if(r<0 || r>=ROWS || c<0 || c>=COLS || grid[r][c] == '#') return;
+
+    if (r == ROWS-1 && c == COLS-2){
+        *longest_pathlength = max(pathlength, *longest_pathlength);
+        return;
+    }
+
+    grid[r][c] = '#';
+    backtrack_part2(grid, ROWS, COLS, r, c+1, pathlength+1, longest_pathlength);
+    backtrack_part2(grid, ROWS, COLS, r, c-1, pathlength+1, longest_pathlength);
+    backtrack_part2(grid, ROWS, COLS, r+1, c, pathlength+1, longest_pathlength);
+    backtrack_part2(grid, ROWS, COLS, r-1, c, pathlength+1, longest_pathlength);
+    grid[r][c] = '.';
 }
 
 int main(int argc, char **argv)
@@ -48,8 +64,11 @@ int main(int argc, char **argv)
     int ROWS = read_file_to_lines(&grid, file_path);
     int COLS = strlen(grid[0]);
 
-    size_t longest_pathlength = 0;
-    backtrack(grid, ROWS, COLS, 0, 1, 0, &longest_pathlength);
-    printf("part1 sol: %zu\n", longest_pathlength);
+    size_t longest_pathlength_part1 = 0;
+    backtrack_part1(grid, ROWS, COLS, 0, 1, 0, &longest_pathlength_part1);
+    printf("part1 sol: %zu\n", longest_pathlength_part1);
 
+    size_t longest_pathlength_part2 = 0;
+    backtrack_part2(grid, ROWS, COLS, 0, 1, 0, &longest_pathlength_part2);
+    printf("part2 sol: %zu\n", longest_pathlength_part2);
 }
