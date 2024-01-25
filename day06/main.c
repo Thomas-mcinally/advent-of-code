b1d1a3b2213c212e4e6f530a2f316251cbff89bf
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-25 08:20:55 +0000

    refactor day 06

--- day06/main.c
+++ day06/main.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-// assume inputs fit in 64-bit unsigned int
-
-long long unsigned int number_of_ways_to_win(long long unsigned int time_limit, long long unsigned int record_distance)
+size_t number_of_ways_to_win(size_t time_limit, size_t record_distance)
 {
-    long long unsigned int l = 0;
-    long long unsigned int r = time_limit;
+    size_t l = 0;
+    size_t r = time_limit;
     while (l <= r)
     {
-        long long unsigned int mid = l + (r - l) / 2;
-        long long unsigned int distance_traveled = (time_limit - mid) * mid;
+        size_t mid = l + (r - l) / 2;
+        size_t distance_traveled = (time_limit - mid) * mid;
         if (distance_traveled > record_distance)
             r = mid - 1;
         else
@@ -22,19 +20,18 @@ int main()
 {
     int times[] = {59, 79, 65, 75};
     int distances[] = {597, 1234, 1032, 1328};
-    int part1_result = 1;
+    size_t part1_result = 1;
     for (int i = 0; i < 4; i++)
     {
-        int number_of_ways = number_of_ways_to_win(times[i], distances[i]);
-        part1_result *= number_of_ways;
+        part1_result *= number_of_ways_to_win(times[i], distances[i]);
     }
 
-    printf("Part1 sol: %i\n", part1_result);
+    printf("Part1 sol: %zu\n", part1_result);
 
-    long long unsigned int corrected_time = 59796575;
-    long long unsigned int corrected_distance = 597123410321328;
-    long long unsigned int part2_result = number_of_ways_to_win(corrected_time, corrected_distance);
-    printf("Part2 sol: %llu\n", part2_result);
+    size_t part_2_time = 59796575;
+    size_t part_2_distance = 597123410321328;
+    size_t part2_result = number_of_ways_to_win(part_2_time, part_2_distance);
+    printf("Part2 sol: %zu\n", part2_result);
 
     return 0;
 }
