43c6da77323455e746e71a79056ab8cb6f952153
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-28 12:46:00 +0000

    refactor filename

--- day02/tests/tests.sh
+++ day02/tests/tests.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")"
 source ../../test_utils/run_test.sh
 
 echo "Running tests for day02"
-run_test "part1" "./input1.txt" "../main.c" "part1 solution: 8"
-run_test "part2" "./input1.txt" "../main.c" "part2 solution: 2286"
+run_test "part1" "./test_input.txt" "../main.c" "part1 solution: 8"
+run_test "part2" "./test_input.txt" "../main.c" "part2 solution: 2286"
