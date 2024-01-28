46cb4cb9b0e603f6ccb8ea748360e70a91eac910
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-28 20:08:07 +0000

    correct day01 tests

--- day01/tests/tests.sh
+++ day01/tests/tests.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 source ../../test_utils/run_test.sh
 
 echo "Running tests for day01"
-if ! run_test "part1" "./input1.txt" "../main.c" "Part1 sol: 145"; then
+if ! run_test "part1" "./input1.txt" "../main.c" "Part1 sol: 142"; then
     exit 1
 fi
 
