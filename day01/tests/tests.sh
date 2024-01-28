bf66a043353cb80eaaa1a04a763eb285493edd9a
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-28 19:57:35 +0000

    check test fails

--- day01/tests/tests.sh
+++ day01/tests/tests.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 source ../../test_utils/run_test.sh
 
 echo "Running tests for day01"
-run_test "part1" "./input1.txt" "../main.c" "Part1 sol: 142"
+run_test "part1" "./input1.txt" "../main.c" "Part1 sol: 143"
 if [ $? -ne 0 ]; then
     exit 1
 fi
