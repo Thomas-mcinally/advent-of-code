1e7446fb78b3c1584eb7cc2fb6c90cb7272c49fe
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-28 10:41:43 +0000

    fix test scripts

--- day01/tests/test_main.sh
+++ day01/tests/test_main.sh
@@ -5,6 +5,7 @@ input_file_path="./input1.txt"
 build_command="gcc ../main.c ../../libs/aoc_lib.c -I ../../libs"
 expected_output="Part1 sol: 142"
 
+cd "$(dirname "$0")"
 eval $build_command
 output=$(./a.out $input_file_path)
 
--- day01/tests/test_main2.sh
+++ day01/tests/test_main2.sh
@@ -5,6 +5,7 @@ input_file_path="./input2.txt"
 build_command="gcc ../main2.c ../../libs/aoc_lib.c -I ../../libs"
 expected_output="Part2 sol: 281"
 
+cd "$(dirname "$0")"
 eval $build_command
 output=$(./a.out $input_file_path)
 
