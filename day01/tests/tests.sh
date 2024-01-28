d543bd22380f75070d12798ae51d832de39b1096
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-28 19:56:41 +0000

    handle error code day01

--- day01/tests/tests.sh
+++ day01/tests/tests.sh
@@ -5,4 +5,11 @@ source ../../test_utils/run_test.sh
 
 echo "Running tests for day01"
 run_test "part1" "./input1.txt" "../main.c" "Part1 sol: 142"
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 run_test "part2" "./input2.txt" "../main2.c" "Part2 sol: 281"
+if [ $? -ne 0 ]; then
+    exit 1
+fi
