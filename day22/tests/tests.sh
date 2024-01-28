fa1e20b0dba1666f94fd61fa626acae3856db8e2
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-28 16:19:06 +0000

    add day22 tests

---
+++ day22/tests/tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+source ../../test_utils/run_test.sh
+
+echo "Running tests for day22"
+run_test "part1" "./test_input.txt" "../main.c" "part1 solution: 5"
+run_test "part2" "./test_input.txt" "../main.c" "part2 solution: 7"
