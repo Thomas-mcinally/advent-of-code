3e99a87a8fe24ae19cd18ed4015d1a383f94761e
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-29 07:45:29 +0000

    correct day01

--- day01/tests/tests.sh
+++ day01/tests/tests.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$0")"
 source ../../test_utils/run_test.sh
 
 echo "Running tests for day01"
-run_test "part1" "./input1.txt" "../main.c" "Part1 sol: 143"
+run_test "part1" "./input1.txt" "../main.c" "Part1 sol: 142"
 run_test "part2" "./input2.txt" "../main2.c" "Part2 sol: 281"
