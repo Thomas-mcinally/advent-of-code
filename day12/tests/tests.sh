8712d8cbc1cffb602ac07c6928b39299fe0bc2a3
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-29 19:36:54 +0000

    make testcase a bit better

--- day12/tests/tests.sh
+++ day12/tests/tests.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$0")"
 source ../../test_utils/run_test.sh
 
 echo "Running tests for day12"
-run_test "part1" "./test_input.txt" "../main.c" "Part1 sol: 1"
-run_test "part2" "./test_input.txt" "../main.c" "Part2 sol: 1"
+run_test "part1" "./test_input.txt" "../main.c" "Part1 sol: 5"
+run_test "part2" "./test_input.txt" "../main.c" "Part2 sol: 16385"
