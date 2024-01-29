2b91e1d5c79398b24a76d87d09780bda5003fd94
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-29 14:46:50 +0000

    remove day12 test

--- day12/tests/tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-cd "$(dirname "$0")"
-
-source ../../test_utils/run_test.sh
-
-echo "Running tests for day12"
-run_test "part1" "./test_input.txt" "../main.c" "Part1 sol: 21"
-run_test "part2" "./test_input.txt" "../main.c" "Part2 sol: 525152"
