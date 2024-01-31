0c7180bbe23dec543d66150087a0f3d8b4e3a758
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-01-31 18:25:30 +0000

    day25 - kergers algo mincut implemented with python

--- day25/main.c
+++ day25/main.c
@@ -10,23 +10,19 @@
 
 typedef struct {
     char *key;
-    char **value;
+    int value; // edge-count
+} Neighbour_To_Edge_Count;
+
+typedef struct {
+    char *key;
+    Neighbour_To_Edge_Count *value;
 } Node_To_Neighbours;
 
-void dfs(Node_To_Neighbours *adjacency_list, char *node, int *is_node_visited, size_t *graph_size) {
-    int node_i = shgeti(adjacency_list, node);
-    if (is_node_visited[node_i]) {
-        return;
-    }
+typedef struct {
+    char *key;
+    int value;
+} Node_To_Absorbed_Node_Count;
 
-    is_node_visited[node_i] = 1;
-    (*graph_size)++;
-
-    char **neighbours = shget(adjacency_list, node);
-    for (int i = 0; i < arrlen(neighbours); i++) {
-        dfs(adjacency_list, neighbours[i], is_node_visited, graph_size);
-    }
-}
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -34,64 +30,89 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    char *file_path = argv[1];
-    char **lines = NULL;
-    int lineCount = read_file_to_lines(&lines, file_path);
-    Node_To_Neighbours *adjacency_list = NULL;
-    shdefault(adjacency_list, NULL);
-    for (int i = 0; i < lineCount; i++) {
-        char *separator = strchr(lines[i], ':');
-        *separator = '\0';
 
-        char **neighbours = NULL;
-        int neighbourCount = split_string_by_delimiter_string(separator + 2, " ", &neighbours);
 
-        for (int j = 0; j < neighbourCount; j++) {
-            char *a = lines[i];
-            char *b = neighbours[j];
-            char **a_neighbours = shget(adjacency_list, a);
-            char **b_neighbours = shget(adjacency_list, b);
+        int min_cut = 20000;
+        int iterations = 0;
+        int partition1, partition2;
+        while(iterations<1000){
 
-            arrput(a_neighbours, b);
-            arrput(b_neighbours, a);
+            //build adjacency list
+            char *file_path = argv[1];
+            char **lines = NULL;
+            int lineCount = read_file_to_lines(&lines, file_path);
+            Node_To_Neighbours *adjacency_list = NULL;
+            shdefault(adjacency_list, NULL);
+            for (int i = 0; i < lineCount; i++) {
+                char *separator = strchr(lines[i], ':');
+                *separator = '\0';
 
-            shput(adjacency_list, a, a_neighbours);
-            shput(adjacency_list, b, b_neighbours);
+                char **neighbours = NULL;
+                int neighbourCount = split_string_by_delimiter_string(separator + 2, " ", &neighbours);
+                for (int j = 0; j < neighbourCount; j++) {
+                    char *a = lines[i];
+                    char *b = neighbours[j];
+
+                    Neighbour_To_Edge_Count *a_neighbours = shget(adjacency_list, a);
+                    Neighbour_To_Edge_Count *b_neighbours = shget(adjacency_list, b);
+                    shput(a_neighbours, b, 1);
+                    shput(b_neighbours, a, 1);
+                    shput(adjacency_list, a, a_neighbours);
+                    shput(adjacency_list, b, b_neighbours);
+                }
+            }
+
+            Node_To_Absorbed_Node_Count *node_to_absorbed_nodes = NULL;
+            for (int i=0; i<shlen(adjacency_list); i++) shput(node_to_absorbed_nodes, adjacency_list[i].key, 1);
+
+
+            while (shlen(adjacency_list) > 2){
+                char *start = adjacency_list[rand() % shlen(adjacency_list)].key;
+                Neighbour_To_Edge_Count *start_neighbours = shget(adjacency_list, start);
+
+                char *finish = start_neighbours[rand() % shlen(start_neighbours)].key;
+                Neighbour_To_Edge_Count *finish_neighbours = shget(adjacency_list, finish);
+
+                for (int i=0; i<shlen(finish_neighbours); i++){
+                    char *key = finish_neighbours[i].key;
+                    int value = finish_neighbours[i].value;
+                    Neighbour_To_Edge_Count *key_neighbours = shget(adjacency_list, key);
+                    if (shgeti(key_neighbours, finish) != -1) shdel(key_neighbours, finish);
+                    shput(adjacency_list, key, key_neighbours);
+
+                    if (strcmp(key, start) == 0) continue;
+
+                    int cur_val = shget(start_neighbours, key);
+                    shput(start_neighbours, key, cur_val + value);
+                    key_neighbours = shget(adjacency_list, key);
+                    int cur_val_2 = shget(key_neighbours, start);
+                    shput(key_neighbours, start, cur_val_2 + value);
+                }
+
+                int absorbed_nodes = shget(node_to_absorbed_nodes, finish);
+                int absorbed_nodes_2 = shget(node_to_absorbed_nodes, start);
+                shput(node_to_absorbed_nodes, start, absorbed_nodes + absorbed_nodes_2);
+                if (shgeti(node_to_absorbed_nodes, finish) != -1 ) shdel(node_to_absorbed_nodes, finish);
+                if (shgeti(adjacency_list, finish) != -1 ) shdel(adjacency_list, finish);
+            }
+
+            iterations++;
+            char *node1 = adjacency_list[0].key;
+            char *node2 = adjacency_list[1].key;
+            Neighbour_To_Edge_Count *node1_neighbours = shget(adjacency_list, node1);
+            int cut = shget(node1_neighbours, node2);
+            if (cut < min_cut){
+                min_cut = cut;
+                partition1 = shget(node_to_absorbed_nodes, node1);
+                partition2 = shget(node_to_absorbed_nodes, node2);
+            }
+
         }
+        printf("Solution min_cut %d\n", min_cut);
+        printf("Solution partition1 %d\n", partition1);
+        printf("Solution partition2 %d\n", partition2);
+        printf("Partiton1 * partiton2 %d\n", partition1 * partition2);
     }
 
-    int *is_node_visited = calloc(shlen(adjacency_list), sizeof(int));
-    size_t graph1_size = 0;
-    dfs(adjacency_list, "mhb", is_node_visited, &graph1_size);
-    size_t graph2_size = 0;
-    dfs(adjacency_list, "zqg", is_node_visited, &graph2_size);
 
-    printf("Graph 1 size: %zu\n", graph1_size);
-    printf("Graph 2 size: %zu\n", graph2_size);
-
-    printf("Part1 sol: %zu\n", graph1_size * graph2_size);
-
-
-}
-
-
-
-// approach:
-// By inspecting graph visually, found that my linkers are mhb/zqg, sjr/jlt, fjn/mzb
-// Removed these edges then explored both graphs to find nr. of nodes in each. Startpoints: mhb and zqg
-
-
-
-
-// Proper approach: Karger's Min-cut algorithm
-// https://en.wikipedia.org/wiki/Karger%27s_algorithm
-// 
-
-// PSEUDOCODE:
-// while len(vertices) > 2:
-//  random_edge = rand(graph)
-//  random_edge.u.neighbours.extend(random_edge.v.neighbours) // u will now have itself as a neighbour
-//  Update adjacency list so nothing points to v anymore (might be hard)
-
-
-// Think best implementation probably uses a union-find data structure
+// Still not working :(
--- day25/main.py
+++ day25/main.py
@@ -1,12 +1,9 @@
 from collections import defaultdict
 from copy import deepcopy
-from random import randint
+import random
 
 # General solution possible with Kergers mincut algo
 # Implement Kergers in python first to get a working version, do it in C afterwards
-
-# Doesnt work yet :()
-
 
 adjacency_list = defaultdict(lambda: defaultdict(int))  # {node: {neighbor: edge_count}}
 input_lines = open('input.txt').read().splitlines()
@@ -20,16 +17,20 @@ for line in input_lines:
         adjacency_list[neighbour][start] += 1
 
 
-min_cut = 0
+
+min_cut = float("inf")
+partition1 = 0
+partition2 = 0
 iterations = 0
-while iterations < 1000 and min_cut != 3:
+while iterations < 100:
     adjacency_list_copy = deepcopy(adjacency_list)
+    node_to_absorbed_nodes = {key: 1 for key in adjacency_list}
     while len(adjacency_list_copy) > 2:
         # select random key from adjacency list
-        start = list(adjacency_list_copy.keys())[randint(0, len(adjacency_list_copy) - 1)]
+        start = random.choice(list(adjacency_list_copy.keys()))
         start_neighbours = adjacency_list_copy[start]
 
-        finish = list(start_neighbours.keys())[randint(0, len(start_neighbours) - 1)]
+        finish = random.choice(list(start_neighbours.keys()))
         finish_neighbours = adjacency_list_copy[finish]
 
         # make neighbours of finish neighbours of start instead
@@ -40,14 +41,19 @@ while iterations < 1000 and min_cut != 3:
             adjacency_list_copy[key][start] += value
 
 
+        node_to_absorbed_nodes[start] += node_to_absorbed_nodes[finish]
         del adjacency_list_copy[finish]
+        del node_to_absorbed_nodes[finish]
     
     iterations += 1
     node1, node2 = [key for key in adjacency_list_copy.keys()]
-    min_cut = adjacency_list_copy[node1][node2]
+    if adjacency_list_copy[node1][node2] < min_cut:
+        min_cut = adjacency_list_copy[node1][node2]
+        partition1 = node_to_absorbed_nodes[node1]
+        partition2 = node_to_absorbed_nodes[node2]
 
 
-
-
+print("min cut found")
+print(partition1*partition2)
 
 
