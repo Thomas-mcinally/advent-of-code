3063eb3916cf73cb312842622bf9b592dd03af9d
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-02-03 19:44:02 +0000

    comment out day10 tests

--- day10/tests/tests.sh
+++ day10/tests/tests.sh
@@ -5,6 +5,6 @@ cd "$(dirname "$0")"
 source ../../test_utils/run_test.sh
 
 echo "Running tests for day10"
-run_test "part1" "./test_input.txt" "../main.c" "Part1 sol: 80"
-run_test "part2" "./test_input.txt" "../main.c" "Part2 sol: 10"
+# run_test "part1" "./test_input.txt" "../main.c" "Part1 sol: 80"
+# run_test "part2" "./test_input.txt" "../main.c" "Part2 sol: 10"
 
