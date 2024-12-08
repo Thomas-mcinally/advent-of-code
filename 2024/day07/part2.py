c029e61a3e02a32d53edd087051e468bb385ea15
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-12-08 20:53:24 +0000

    day07

---
+++ 2024/day07/part2.py
@@ -0,0 +1,31 @@
+import sys
+import time
+sys.setrecursionlimit(10000)
+
+with open("input.txt") as f:
+    s = f.read().strip()
+lines = s.splitlines()
+
+def can_be_correct_starting_from(target: int, operands: list[int], amount: int, i: int) -> bool:
+    # caching by (amount, i) not worth it because range of amount is so large
+    if i == len(operands):
+        return amount == target
+    if amount > target:
+        return False
+
+    multiply_works = can_be_correct_starting_from(target, operands, amount*operands[i], i+1)
+    adding_works = can_be_correct_starting_from(target, operands, amount+operands[i], i+1)
+    concat_works = can_be_correct_starting_from(target, operands, int(str(amount)+str(operands[i])), i+1)
+    return  multiply_works or adding_works or concat_works
+
+
+part2 = 0
+for line in lines:
+    colon_idx = line.index(":")
+    result = int(line[:colon_idx])
+
+    operands = [int(item) for item in line[colon_idx+2:].split(" ")]
+
+    if can_be_correct_starting_from(result, operands, operands[0], 1):
+        part2 += result
+print(part2)
