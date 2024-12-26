6e321d358169ffc2db5993d9a3673980cf918ba5
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-12-26 17:47:09 +0100

    update day24 part2

--- 2024/day24/part2.py
+++ 2024/day24/part2.py
@@ -2,12 +2,11 @@
 Solved graphically, see part2_before.dot and part2_after.dot
 Good problem overview: https://www.reddit.com/r/adventofcode/comments/1hla5ql/2024_day_24_part_2_a_guide_on_the_idea_behind_the/
 
-Discarded idea: Brute force?
+Discarded idea: Brute force
 ~200 gates
 need to swap 4pairs (8 in total)
-200 ways to choose nr1
-199 ways to choose nr2
-...
+200 ways to choose nr1, 199 ways to choose nr2, ...
+
 basically 200**8 ways to swap 4 pairs
 --> can't brute force, it will take too long to finish computing the result
 
